d95c4d0dae27f0b2c87e8cb7473a9a376b25d49e
decodes02 <deepakcse2025@gmail.com>
2025-02-14 22:15:22 +0530

    Checking for new files

--- first.kt
+++ first.kt
@@ -1,8 +1,8 @@
 fun main(){
     for((i,v) in (1..6).withIndex()){
-        println("$i $v");
+        println("$i $v")
     }
-    val x = 10;
+    val x = 10
     print(x)
 
     print("Checking changes")
